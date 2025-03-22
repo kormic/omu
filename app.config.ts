64cd6991221492b924448d8ca3b1fadb2b5c35a2
Michael Kornelakis <kornelakism@gmail.com>
2025-03-22 17:59:22 +0200

    remove app.json

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,40 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 dotenv.config();
 
-const firebase = JSON.parse(process.env.FIREBASE!);
+const firebase = process.env.FIREBASE ? JSON.parse(process.env.FIREBASE) : {};
 
 module.exports = {
-    name: 'OmuApp',
-    version: '0.0.1',
+  expo: {
+    name: 'omu',
+    slug: 'omu',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    newArchEnabled: true,
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#FFFFFF',
+      },
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
     extra: {
-        firebase: firebase
-    }
-}
+      firebase: firebase,
+    },
+  },
+};
