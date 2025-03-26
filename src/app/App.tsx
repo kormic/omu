663e6f89f4cca1986fd309d009ceb66e2d0e35f5
Michael Kornelakis <kornelakism@gmail.com>
2025-03-26 22:16:54 +0200

    add login screen

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,71 +1,23 @@
 import { registerRootComponent } from "expo";
-import { StatusBar } from "expo-status-bar";
-import { User, AuthError } from "firebase/auth/react-native";
-import { useState } from "react";
 import {
+  SafeAreaView,
   StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
 } from "react-native";
-import { initialize } from "../../config/firebase";
-
-const { signInWithEmailAndPassword, auth, onAuthStateChanged, signOut } =
-  initialize();
+import Login from "./screens/Login/components/Login";
 
 function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState<User | null>(null);
-  const [error, setError] = useState<string | null>(null);
-
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      setError(null);
-    }
-
-    setUser(user);
-  });
-
-  const handleLogin = async () => {
-    signInWithEmailAndPassword(auth, email, password).catch((e: AuthError) => {
-      setError(e.message);
-    });
-  };
 
   return (
-    <View style={styles.container}>
-      {error && <Text>{error}</Text>}
-      {user ? <Text>{user.displayName}</Text> : <Text>Not Logged in</Text>}
-      <View>
-        <TextInput value={email} onChangeText={(text) => setEmail(text)} />
-        <TextInput
-          secureTextEntry
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-        />
-        {!user ? (
-          <TouchableOpacity onPress={handleLogin}>
-            <Text>Login</Text>
-          </TouchableOpacity>
-        ) : (
-          <TouchableOpacity onPress={() => signOut(auth)}>
-            <Text>Logout</Text>
-          </TouchableOpacity>
-        )}
-      </View>
-      <StatusBar style='auto' />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Login />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#1E1E1E'
   },
 });
 
---
+++ src/app/screens/Login/components/Login.tsx
@@ -0,0 +1,156 @@
+import { AuthError } from "firebase/auth/react-native";
+import { useState } from "react";
+import {
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+    Image,
+} from "react-native";
+import { initialize } from "../../../../../config/firebase";
+
+const { signInWithEmailAndPassword, auth, onAuthStateChanged, signOut } =
+    initialize();
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [_, setError] = useState<string | null>(null);
+
+    const handleLogin = async () => {
+        signInWithEmailAndPassword(auth, email, password).catch((e: AuthError) => {
+            setError(e.message);
+        });
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image style={styles.logo} source={require('../../../../../assets/omu.png')}></Image>
+                <Text style={[styles.text, styles.headerTitle]}>Login</Text>
+                <View style={styles.logo}></View>
+            </View>
+            <View style={styles.welcomeContainer}>
+                <View style={styles.welcomeTextContainer}>
+                    <Text style={[styles.text, styles.welcomeText]}>Welcome to JivApp</Text>
+                    <Text style={styles.text}>I want to be a Taker</Text>
+                    <Text style={styles.text}>I want to receive a Jiver</Text>
+                </View>
+                <Image style={styles.welcomeImage} source={require('../../../../../assets/jivers.png')}></Image>
+            </View>
+            <View style={styles.loginForm}>
+                <TextInput
+                    placeholder="Email / Phone number / Username"
+                    style={styles.username}
+                    placeholderTextColor={'#969696'}
+                    value={email}
+                    onChangeText={setEmail}
+                    keyboardType="email-address"
+                />
+                <TextInput
+                    style={styles.password}
+                    placeholder="Password"
+                    placeholderTextColor={'#969696'}
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                />
+                <TouchableOpacity style={styles.forgotPassword}>
+                    <Text style={styles.text}>Forgot Password?</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.loginContainer}>
+                <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+                    <Text style={[styles.text, styles.loginText]}>Login</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.registerContainer}>
+                <Text style={styles.text}>Don't have an account yet?</Text>
+                <TouchableOpacity style={styles.registerButton}>
+                    <Text style={styles.text}>Register here!</Text>
+                </TouchableOpacity>
+            </View>
+        </View >
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#1E1E1E',
+        padding: 20
+    },
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    logo: {
+        aspectRatio: 1,
+        width: 40
+    },
+    text: {
+        color: "#FFF",
+    },
+    headerTitle: {
+        fontSize: 16
+    },
+    welcomeContainer: {
+        padding: 35
+    },
+    welcomeTextContainer: {
+        paddingBottom: 35
+    },
+    welcomeText: {
+        paddingBottom: 12
+    },
+    welcomeImage: {
+        backgroundColor: 'white',
+        alignSelf: "center"
+    },
+    loginForm: {
+        gap: 12
+    },
+    loginContainer: {
+        marginTop: 25
+    },
+    loginButton: {
+        // paddingTop: 35,
+        height: 59,
+        borderRadius: 15,
+        backgroundColor: '#FAA61A',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    loginText: {
+        fontSize: 17,
+        fontWeight: '700'
+    },
+    username: {
+        height: 56,
+        borderColor: "#FFF",
+        borderWidth: 1,
+        borderRadius: 10,
+        padding: 10,
+        marginBottom: 10
+    },
+    password: {
+        height: 56,
+        borderColor: "#FFF",
+        borderWidth: 1,
+        borderRadius: 10,
+        padding: 10,
+        marginBottom: 10
+    },
+    forgotPassword: {
+        alignSelf: 'flex-end'
+    },
+    registerContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 35
+    },
+    registerButton: {
+        paddingLeft: 5
+    }
+});
