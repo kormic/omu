f4ed94d17808646a772da187ae8cfa04ba7dbb41
Michael Kornelakis <kornelakism@gmail.com>
2025-11-02 21:35:27 +0200

    change register page animation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,14 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { AuthProvider } from '../src/features/auth/hooks/useAuth';
-import { Slot } from 'expo-router';
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
 	return (
 		<SafeAreaView style={styles.container}>
 			<StatusBar style="auto" />
 			<AuthProvider>
-				<Slot
+				<Stack
 					screenOptions={{
 						headerShown: false,
 						contentStyle: {
@@ -17,7 +17,21 @@ export default function RootLayout() {
 							backgroundColor: '#1E1E1E',
 						},
 					}}
-				/>
+				>
+					{/* Home (default) */}
+					<Stack.Screen name="index" />
+
+					<Stack.Screen name="(auth)/login" />
+
+					<Stack.Screen
+						name="(auth)/register"
+						options={{
+							presentation: 'modal',
+							animation: 'slide_from_bottom',
+							gestureEnabled: true,
+						}}
+					/>
+				</Stack>
 			</AuthProvider>
 		</SafeAreaView>
 	);
